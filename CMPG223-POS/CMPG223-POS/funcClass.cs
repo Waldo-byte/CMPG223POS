3a40b8582397721d82b9a9e46255936bf668ebdd
Estian Eek <eek.estian@yahoo.com>
2020-10-12 12:05:33 +0200

    Estian func commit

---
+++ CMPG223-POS/CMPG223-POS/funcClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPG223_POS
+{
+    class funcClass
+    {
+        public String addItem(String item)
+        {
+             
+        }
+
+        public void deleteItem(String item)
+        {
+
+        }
+
+        public String placeOrder(String[] orders, int tableNum)
+        {
+
+        }
+
+        public void addStock(String item)
+        {
+
+        }
+
+        public void removeStock(String item)
+        {
+
+        }
+
+        public void buyStock(Stock[] buyStock)
+        {
+
+        }
+
+        public void payBill(int tableNum)
+        {
+
+        }
+
+        public void clockIn(int waiaterID)
+        {
+
+        }
+
+        public void clockOut(int waiterID)
+        {
+
+        }
+
+
+
+
+    }
+}
