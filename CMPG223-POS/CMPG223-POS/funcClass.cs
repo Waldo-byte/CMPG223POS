f216a77783f8f1b499174741ed9578efdf630cf3
Estian Eek <eek.estian@yahoo.com>
2020-10-19 11:31:55 +0200

    Estian placeOrder function

--- CMPG223-POS/CMPG223-POS/funcClass.cs
+++ CMPG223-POS/CMPG223-POS/funcClass.cs
@@ -1,40 +1,66 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CMPG223_POS
 {
     class funcClass
     {
-        //public String addItem(String item)
-        //{
-             
-        //}
+        static String constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Waldo-byte\CMPG223-POS\CMPG223-POS\CMPG223-POS\Route96.mdf;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(constr);
+        SqlCommand comm;
+        SqlDataAdapter adap;
+        DataSet ds;
+        public String addItem(String item)
+        {
 
-        //public void deleteItem(String item)
-        //{
+        }
 
-        //}
+        public void deleteItem(String item)
+        {
 
-        //public String placeOrder(String[] orders, int tableNum)
-        //{
+        }
 
-        //}
+        public decimal placeOrder(String[] orders, int tableNum)
+        {
+            conn.Open();
+            int ammount;
+            for (int i = 0; i < orders.Length; i++)
+            {
+                String sqlGet = "SELECT Bought_Inv FROM Menu_Item WHERE Inventory_ID = '" + orders[i] + "'";
+                comm = new SqlCommand(sqlGet, conn);
+                adap = new SqlDataAdapter();
+                ds = new DataSet();
+                adap.SelectCommand = comm;
+                adap.Fill(ds);
+                SqlDataReader reader = comm.ExecuteReader();
+                if (reader.Read())
+                {
+                    int.TryParse((string)reader[0], out ammount);
+                }
+                else
+                {
+                    int.TryParse((string)reader[0], out ammount);
+                }
 
+                String sqlSet = "UPDATE Menu_Item SET Bought_Inv = '" + (ammount - 1) + "'" + " WHERE Inventory_ID = " + orders[i] + "'";
+            }
+        }
         public void addStock(String item)
         {
 
-        //}
+        }
 
-        //public void removeStock(String item)
-        //{
+        public void removeStock(String item)
+        {
 
-        //}
+        }
 
         public void buyStock(Stock[] buyStock)
-        {
 
         //}
 
@@ -55,6 +81,5 @@ namespace CMPG223_POS
 
 
 
-
     }
-}*/
+}
