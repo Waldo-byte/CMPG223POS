6ba8f3cbe1096a6fea47468bcb950fb9b1d8cd24
Estian Eek <eek.estian@yahoo.com>
2020-10-23 14:31:03 +0200

    Estian Waiter add

--- CMPG223-POS/CMPG223-POS/WaiterAddForm.Designer.cs
+++ CMPG223-POS/CMPG223-POS/WaiterAddForm.Designer.cs
@@ -40,65 +40,73 @@
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(100, 59);
+            this.lblName.Location = new System.Drawing.Point(75, 48);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(87, 17);
+            this.lblName.Size = new System.Drawing.Size(66, 13);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Enter Name:";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(76, 99);
+            this.lblPassword.Location = new System.Drawing.Point(57, 80);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(111, 17);
+            this.lblPassword.Size = new System.Drawing.Size(84, 13);
             this.lblPassword.TabIndex = 1;
             this.lblPassword.Text = "Enter Password:";
             // 
             // lblConfirmPassword
             // 
             this.lblConfirmPassword.AutoSize = true;
-            this.lblConfirmPassword.Location = new System.Drawing.Point(55, 142);
+            this.lblConfirmPassword.Location = new System.Drawing.Point(41, 115);
+            this.lblConfirmPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblConfirmPassword.Name = "lblConfirmPassword";
-            this.lblConfirmPassword.Size = new System.Drawing.Size(132, 17);
+            this.lblConfirmPassword.Size = new System.Drawing.Size(99, 13);
             this.lblConfirmPassword.TabIndex = 2;
             this.lblConfirmPassword.Text = "Confirm Passsword:";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(193, 56);
+            this.txtName.Location = new System.Drawing.Point(145, 46);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(141, 22);
+            this.txtName.Size = new System.Drawing.Size(107, 20);
             this.txtName.TabIndex = 3;
             // 
             // txtConfirmPassword
             // 
-            this.txtConfirmPassword.Location = new System.Drawing.Point(193, 142);
+            this.txtConfirmPassword.Location = new System.Drawing.Point(145, 115);
+            this.txtConfirmPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtConfirmPassword.Name = "txtConfirmPassword";
-            this.txtConfirmPassword.Size = new System.Drawing.Size(141, 22);
+            this.txtConfirmPassword.Size = new System.Drawing.Size(107, 20);
             this.txtConfirmPassword.TabIndex = 4;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(193, 99);
+            this.txtPassword.Location = new System.Drawing.Point(145, 80);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(141, 22);
+            this.txtPassword.Size = new System.Drawing.Size(107, 20);
             this.txtPassword.TabIndex = 5;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(210, 196);
+            this.btnAdd.Location = new System.Drawing.Point(158, 159);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(93, 45);
+            this.btnAdd.Size = new System.Drawing.Size(70, 37);
             this.btnAdd.TabIndex = 6;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // WaiterAddForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 268);
+            this.ClientSize = new System.Drawing.Size(387, 218);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtConfirmPassword);
@@ -106,6 +114,7 @@
             this.Controls.Add(this.lblConfirmPassword);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "WaiterAddForm";
             this.Text = "WaiterAddForm";
             this.ResumeLayout(false);
--- CMPG223-POS/CMPG223-POS/WaiterAddForm.cs
+++ CMPG223-POS/CMPG223-POS/WaiterAddForm.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CMPG223_POS
 {
     public partial class WaiterAddForm : Form
     {
+
+        static string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Waldo-byte\CMPG223-POS\CMPG223-POS\CMPG223-POS\Route96.mdf;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(constr);
+        SqlCommand comm;
+        SqlDataAdapter adap;
+        DataSet ds;
         public WaiterAddForm()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(txtPassword.Text == txtConfirmPassword.Text)
+            {
+                string sql = @"INSERT INTO Waiter (Password, FirstName, LastName) VALUES ('" + txtPassword.Text + "', '" + txtName.Text + "', '" + txtSurname.Text + "'";
+
+                comm = new SqlCommand(sql, conn);
+                adap = new SqlDataAdapter();
+                ds = new DataSet();
+
+                adap.SelectCommand = comm;
+                adap.Fill(ds);
+            }
         }
     }
 }
