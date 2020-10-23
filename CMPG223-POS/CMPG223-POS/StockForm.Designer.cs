4b56f85c054187ddd6f26c50d732ebc41ac770ae
Janus Maritz <janusmaritz7@gmail.com>
2020-10-23 13:28:17 +0200

    Added StockForm

---
+++ CMPG223-POS/CMPG223-POS/StockForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CMPG223_POS
+{
+    partial class StockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.dataGridViewStock = new System.Windows.Forms.DataGridView();
+            this.comboBoxItemId = new System.Windows.Forms.ComboBox();
+            this.lblItemId = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtStockPrice = new System.Windows.Forms.TextBox();
+            this.lblStockPrice = new System.Windows.Forms.Label();
+            this.numUpDownStock = new System.Windows.Forms.NumericUpDown();
+            this.lblAmount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 144);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(166, 144);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(311, 144);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(471, 144);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(75, 23);
+            this.btnBackup.TabIndex = 3;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewStock
+            // 
+            this.dataGridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStock.Location = new System.Drawing.Point(12, 173);
+            this.dataGridViewStock.Name = "dataGridViewStock";
+            this.dataGridViewStock.Size = new System.Drawing.Size(534, 150);
+            this.dataGridViewStock.TabIndex = 4;
+            // 
+            // comboBoxItemId
+            // 
+            this.comboBoxItemId.FormattingEnabled = true;
+            this.comboBoxItemId.Location = new System.Drawing.Point(75, 12);
+            this.comboBoxItemId.Name = "comboBoxItemId";
+            this.comboBoxItemId.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxItemId.TabIndex = 5;
+            // 
+            // lblItemId
+            // 
+            this.lblItemId.AutoSize = true;
+            this.lblItemId.Location = new System.Drawing.Point(25, 15);
+            this.lblItemId.Name = "lblItemId";
+            this.lblItemId.Size = new System.Drawing.Size(44, 13);
+            this.lblItemId.TabIndex = 6;
+            this.lblItemId.Text = "Item ID:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(75, 39);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(121, 20);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(6, 42);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(63, 13);
+            this.lblDescription.TabIndex = 8;
+            this.lblDescription.Text = "Description:";
+            // 
+            // txtStockPrice
+            // 
+            this.txtStockPrice.Location = new System.Drawing.Point(75, 65);
+            this.txtStockPrice.Name = "txtStockPrice";
+            this.txtStockPrice.Size = new System.Drawing.Size(121, 20);
+            this.txtStockPrice.TabIndex = 9;
+            // 
+            // lblStockPrice
+            // 
+            this.lblStockPrice.AutoSize = true;
+            this.lblStockPrice.Location = new System.Drawing.Point(34, 68);
+            this.lblStockPrice.Name = "lblStockPrice";
+            this.lblStockPrice.Size = new System.Drawing.Size(34, 13);
+            this.lblStockPrice.TabIndex = 10;
+            this.lblStockPrice.Text = "Price:";
+            // 
+            // numUpDownStock
+            // 
+            this.numUpDownStock.Location = new System.Drawing.Point(75, 92);
+            this.numUpDownStock.Name = "numUpDownStock";
+            this.numUpDownStock.Size = new System.Drawing.Size(120, 20);
+            this.numUpDownStock.TabIndex = 11;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(22, 94);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(46, 13);
+            this.lblAmount.TabIndex = 12;
+            this.lblAmount.Text = "Amount:";
+            // 
+            // StockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 335);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.numUpDownStock);
+            this.Controls.Add(this.lblStockPrice);
+            this.Controls.Add(this.txtStockPrice);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblItemId);
+            this.Controls.Add(this.comboBoxItemId);
+            this.Controls.Add(this.dataGridViewStock);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "StockForm";
+            this.Text = "StockForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.DataGridView dataGridViewStock;
+        private System.Windows.Forms.ComboBox comboBoxItemId;
+        private System.Windows.Forms.Label lblItemId;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtStockPrice;
+        private System.Windows.Forms.Label lblStockPrice;
+        private System.Windows.Forms.NumericUpDown numUpDownStock;
+        private System.Windows.Forms.Label lblAmount;
+    }
+}
