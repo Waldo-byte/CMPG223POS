e5dab1a3673feaaf1012d8a41b7b60cc14b5c5bd
InactiveElement <harding.jaden@gmail.com>
2020-10-25 12:30:14 +0200

    waiter form exception handling

--- CMPG223-POS/CMPG223-POS/Orders.cs
+++ CMPG223-POS/CMPG223-POS/Orders.cs
@@ -10,7 +10,7 @@ namespace CMPG223_POS
     public partial class Orders : Form
     {
 
-        static string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Reyem\source\repos\Waldo-byte\CMPG223-POS\CMPG223-POS\CMPG223-POS\Route96.mdf;Integrated Security=True";
+        static string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Jaden\Desktop\CMPG223 _PROJECT\Main Project\CMPG223-POS\CMPG223-POS\CMPG223-POS\Route96.mdf;Integrated Security=True";
         SqlConnection conn = new SqlConnection(constr);
         SqlCommand comm;
         SqlDataAdapter adap;
